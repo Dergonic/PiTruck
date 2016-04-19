edf50ef4b84540cd38c44940d4ffe20b661e3d7f
Dergonic <nfocant@gmail.com>
2016-04-19 11:34:39 +0200

    V1.0 Truck running

--- app.js
+++ app.js
@@ -1,118 +1,318 @@
 var express = require('express');
 var app = express();
-var http = require('http').Server(app);
-//var    path = require('path');
-//var    async = require('async');
+
+var server = require('http').Server(app);
+var heartbeat={	'pingInterval':200,//ping each 200ms
+				'pingTimeout' :200}// and espect a reply in the next 200 ms (otherwise, close)
+var io = require('socket.io')(server,heartbeat);
+
+
+
+
 var gpio = require('onoff').Gpio;
-var io = require('socket.io')(http);
-var pca = require('./libs/adafruit-pca9685');
 
+var pca = require('/home/pi/PiTruck/libs/adafruit-pca9685.js');
 
-http.listen(3000,function(){
-    console.log('listening to port 3000');
+///////////////////////////////////////////////////
+// Starting http server
+///////////////////////////////////////////////////
+
+server.listen(3000,function(){
+	console.log('listening to port 3000');
 });
 
 app.use(express.static('static'));
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/static/index.html');
+	res.sendFile(__dirname + '/static/index.html');
 });
 
 
-
-
-var led1 = new gpio(14, 'out');
-var led2 = new gpio(16, 'out');
-var led3 = new gpio(20, 'out');
-var led4 = new gpio(21, 'out');
-var pwm_defaults = {
-  "freq":50, // frequency of the device
-  "correctionFactor": 1.118, // correction factor - fine tune the frequency 
-  "address": "0x40", // i2c bus address
-  "device": '/dev/i2c-1', // device name
-  // "debug": <null> // adds some debugging methods if set 
+///////////////////////////////////////////////////
+// Init GPIO and PWM
+///////////////////////////////////////////////////
+function Led(gpionbr, defaultvalue, label) {
+	this.label = label;
+	this.gpio = new gpio(gpionbr, 'out');
+	this.value = defaultvalue;
+	this.resetvalue = defaultvalue;
+	this.gpio.write(this.value);
+}
+Led.prototype.set = function(onoff){
+	this.value = onoff;
+	this.gpio.write(this.value);
 };
-var pwm = pca(pwm_defaults);
+Led.prototype.get = function(){
+	return this.value;
+};
+Led.prototype.close = function(){
+	this.value = this.resetvalue;
+	this.gpio.write(this.value);
+	this.gpio.unexport();
+};
 
-var servo1 = {
-  "channel":0,
-  "min": 1000,
-  "max": 2000
-}
-
-setServoPulse = function(channel, pulse){
-  var pulseLength;
-  pulseLength = 1000000;
-  pulseLength /= 60;
-  //print("%d us per period" % pulseLength);
-  pulseLength /= 4096;
-  //print("%d us per bit" % pulseLength);
-  pulse *= 1000;
-  pulse /= pulseLength;
-  return pwm.setPWM(channel, 0, pulse);
-}
+var led = {};
+led['frein'] = new Led(27, 0, 'Frein');
+led['gdfeu'] = new Led(22, 0, 'Grands feux');
+led['crois'] = new Led(5 , 1, 'Feux de croisement');
+led['arrie'] = new Led(13, 0, 'Feu de recul');
+led['clidr'] = new Led(19, 0, 'Clignotant droit');
+led['cliga'] = new Led(26, 0, 'clignotant gauche');
 
 //var button = new gpio(0, 'in', 'both');
 
-//listen for socket connection
+
+
+
+
+
+var pwm_defaults = {
+	"freq":50, // frequency of the device
+	"correctionFactor": 1.118, // correction factor - fine tune the frequency 
+	"address": "0x40", // i2c bus address
+	"device": '/dev/i2c-1' // device name
+	// "debug": <null> // adds some debugging methods if set 
+};
+var pwm = pca(pwm_defaults);
+/////////////////////////////////////////////////// 
+// function - setServoPulse 
+/////////////////////////////////////////////////// 
+// channel : channel of the servo on the pca 
+// 		from 0 to 15 
+// pulse : pulse length to determine servo orientation 
+// 		value is depending servo model 
+// 		common value from 1000 to 2000 
+///////////////////////////////////////////////////
+
+setServoPulse = function(channel, pulse){
+	try{
+		var pulseLength;
+		pulseLength = 1000000;
+		pulseLength /= 60;
+		//print("%d us per period" % pulseLength);
+		pulseLength /= 4096;
+		//print("%d us per bit" % pulseLength);
+		pulse *= 1000;
+		pulse /= pulseLength;
+		return pwm.setPWM(channel, 0, pulse);
+	}catch(e){console.log('ERROR'+e);}
+}
+
+function Servo(Channel, min, max, defaultvalue){
+	this.channel = Channel;
+	this.min = min;
+	this.max = max;
+	this.resetvalue = defaultvalue;
+	this.value = defaultvalue;
+	pwm.setPulse(this.channel, this.value)
+}
+Servo.prototype.set = function(value){
+	this.value = value;
+	pwm.setPulse(this.channel, this.value);
+};
+Servo.prototype.reset = function(value){
+	this.value = this.resetvalue;
+	pwm.setPulse(this.channel, this.value);
+};
+Servo.prototype.get = function(){
+	return this.value;
+};
+Servo.prototype.getmin = function(){
+	return this.min;
+};
+Servo.prototype.getmax = function(){
+	return this.max;
+};
+Servo.prototype.getresetvalue = function(){
+	return this.resetvalue;
+};
+Servo.prototype.close = function(){
+	this.value=this.resetvalue;
+	pwm.setPulse(this.channel, this.value);
+};
+
+var servo = {};
+servo['direction'] = new Servo(0,1000,2000,1400);
+//servo['propulsion']  = new Servo(1,1000,2000,1500);
+servo['propulsion']  = new Servo(1,1200,1700,1400);
+
+
+
+///////////////////////////////////////////////////
+// socket listeners
+///////////////////////////////////////////////////
+
 io.on('connection', function(socket) {
-  //listen for move signal
-  socket.on('setled1', function(direction) {
-    switch(direction){
-     case 'on':
-        led1.write(1);
-        console.log('send keep led on');
-        break;
-      case 'off':
-        led1.write(0);
-        console.log('send keep led off');
-        break;
-    }
-  });
-  socket.on('setled2', function(direction) {
-    switch(direction){
-     case 'on':
-        led2.write(1);
-        console.log('send temp led on');
-        break;
-      case 'off':
-        led2.write(0);
-        console.log('send temp led off');
-        break;
-    }
-  });
-  socket.on('setservo', function(direction) {
-    switch(direction){
-     case 'min':
-        pwm.setPulse(servo1.channel, servo1.min);
-        console.log('send servo to min');
-        break;
-      case 'half':
-        pwm.setPulse(servo1.channel, (servo1.min+servo1.max)/2);
-        console.log('send servo to half');
-	break;
-      case 'max':
-        pwm.setPulse(servo1.channel, servo1.max);
-        console.log('send servo to max');
-        break;
-    }
-  });
+	log(socket,'CONNECTED'); 
+
+	// Reconnecting ? Recovering from emergency stop
+	if(led['cliga'].get()==1 && led['clidr'].get()==1){ 
+		led['cliga'].set(0);
+		led['clidr'].set(0);
+	}	
+
+	socket.on('feucroisement', function(direction) {
+		switch(direction){
+			case 'on':
+					if(led['crois'].get()!=1) {        
+						led['crois'].set(1);
+					}
+					break;
+			case 'off':
+			default:
+					if(led['crois'].get()!=0) {        
+						led['crois'].set(0);
+					}
+					break;
+		}
+	});
+
+	socket.on('feurecul', function(direction) {
+		switch(direction){
+			case 'on':
+					if(led['arrie'].get()!=1) {        
+						led['arrie'].set(1);
+					}
+					break;
+			case 'off':
+			default:
+					if(led['arrie'].get()!=0) {        
+						led['arrie'].set(0);
+					}
+					break;
+		}
+	});
+	socket.on('grandphares', function(direction) {
+		switch(direction){
+			case 'on':
+					if(led['gdfeu'].get()!=1) {        
+						led['gdfeu'].set(1);
+					}
+					break;
+			case 'off':
+			default:
+					if(led['gdfeu'].get()!=0) {        
+						led['gdfeu'].set(0);
+					}
+					break;
+		}
+	});
+	socket.on('feustop', function(direction) {
+		switch(direction){
+			case 'on':
+					if(led['frein'].get()!=1) {        
+						led['frein'].set(1);
+					}
+					break;
+			case 'off':
+			default:
+					if(led['frein'].get()!=0) {        
+						led['frein'].set(0);
+					}
+					break;
+		}
+	});
+	socket.on('setdirection', function(direction) {
+		switch(direction){
+			case 'droite':
+					if(servo['direction'].get() != servo['direction'].getmin()) {
+						servo['direction'].set(servo['direction'].getmin());
+					}
+					break;
+			case 'gauche':
+					if(servo['direction'].get() != servo['direction'].getmax()) {
+						servo['direction'].set(servo['direction'].getmax());
+					}
+					break;
+			case 'neutre':
+			default:
+					if(servo['direction'].get() != servo['direction'].getresetvalue()) {
+						servo['direction'].reset();
+					}
+					break;
+		}
+	});
+	socket.on('setpropulsion', function(propulsion) {
+		switch(propulsion){
+			case 'min':
+					if(servo['propulsion'].get() != servo['propulsion'].getmin()) {
+						servo['propulsion'].set(servo['propulsion'].getmin());
+					}
+					break;
+			case 'max':
+					if(servo['propulsion'].get() != servo['propulsion'].getmax()) {
+						servo['propulsion'].set(servo['propulsion'].getmax());
+					}
+					break;
+			case 'stop':
+			default:
+					if(servo['propulsion'].get() != servo['propulsion'].getresetvalue()) {
+						servo['propulsion'].reset();
+					}
+					break;
+		}
+	});
+
+
+
+	socket.on('disconnect', function(){
+		log(socket,'DISCONNECTED');
+		
+		if( servo['propulsion'].get() != servo['propulsion'].getresetvalue()  ||
+			servo['direction'].get()  != servo['direction'].getresetvalue() )
+		{
+			log(socket,'EMERGENCY STOP');
+			led['cliga'].set(1);
+			led['clidr'].set(1);
+		}
+		stop();
+	});
+
+	function stop(){ // stopping truck
+		servo['propulsion'].reset();
+		servo['direction'].reset();
+		log(socket,'Truck stopped');
+	}
 });
 
 
+///////////////////////////////////////////////////
+// function - exit
+///////////////////////////////////////////////////
+// Properly exiting the PCA, GPIO and process
+///////////////////////////////////////////////////
 
+function exit(socket){
+	log(socket,'Stopping pca...');
+	for (var ind in servo.length) {
+		servo[ind].close();
+	}
+	pwm.stop();
 
-function exit(){
-    console.log('Stopping pca...');
-    pwm.stop();
+	log(socket,'Unloading gpio ...');
+	for (var ind in led.length) {
+		led[ind].close();
+	}
 
-    console.log('Unloading gpio ...');
-    led1.unexport();
-    led2.unexport();
-    led3.unexport();
-    led4.unexport();
-    
-    console.log('Exiting ...');
-    process.exit(0);
+	log(socket,'Stopping server ...');
+	io.close();
+
+	console.log('Exiting ...');
+	process.exit(0);
 }
+
+
+
+function log(scket, text){
+	console.log(text);
+	if(typeof socket !== 'undefined'){ 
+		socket.emit('log',text);
+	}
+};
+
+
+///////////////////////////////////////////////////
+// grabbing the Ctrl+C to exit properly
+///////////////////////////////////////////////////
+
 process.on('SIGINT', exit);
--- static/js/script.js
+++ static/js/script.js
@@ -1,44 +1,91 @@
 $(function () {
-  var socket = io.connect();
-  var activeClass = 'active';
+	var socket = io.connect();
+	var activeClass = 'active';
 
-  //listen for key presses
-  $(document).keydown(function(e){
-    switch(e.which){
-      case 65:
-        socket.emit('setled1', 'on');
-        $('#BtnA').addClass(activeClass);
-        break;
-      case 90:
-        socket.emit('setled1', 'off');
-        $('#BtnZ').addClass(activeClass);
-        break;
-      case 69:
-	socket.emit('setled2', 'on');
-	$('#BtnE').addClass(activeClass);
-	break;
-      case 81:
-        socket.emit('setservo', 'min');
-        $('#ServoQ').addClass(activeClass);
-        break;
-      case 68:
-        socket.emit('setservo', 'max');
-        $('#ServoD').addClass(activeClass);
-        break;
-    }
-  });
-  
-  $(document).keyup(function(e){
-    $('.btn').removeClass(activeClass);
+	//listen for key presses
+	$(document).keydown(function(e){
+		switch(e.which){
+			case 83: //S
+					if(! $('#BtnAvance').hasClass(activeClass)){
+						socket.emit('feustop','off');
+						socket.emit('feurecul','on');
+						socket.emit('setpropulsion', 'min');
+						$('#BtnRecule').addClass(activeClass);
+					}
+					break;
+			case 90: //Z
+					if(! $('#BtnRecule').hasClass(activeClass)){
+						socket.emit('feustop','off');
+						socket.emit('setpropulsion', 'max');
+						$('#BtnAvance').addClass(activeClass);
+					}
+					break;
+			case 81: //Q
+					if(! $('#BtnDroite').hasClass(activeClass)){
+						socket.emit('setdirection', 'gauche');
+						$('#BtnGauche').addClass(activeClass);
+					}
+					break;
+			case 68: //D
+					if(! $('#BtnGauche').hasClass(activeClass)){
+						socket.emit('setdirection', 'droite');
+						$('#BtnDroite').addClass(activeClass);
+					}
+					break;
+			case 70: //F
+					if($('#BtnCrois').hasClass(activeClass)){
+						socket.emit('feucroisement', 'off');
+						$('#BtnCrois').removeClass(activeClass);
+					}
+					else{
+						socket.emit('feucroisement', 'on');
+						$('#BtnCrois').addClass(activeClass);
+					}
+					break;
+			case 71: //G
+					if($('#BtnGrands').hasClass(activeClass)){
+						socket.emit('grandphares', 'off');
+						$('#BtnGrands').removeClass(activeClass);
+					}
+					else{
+						socket.emit('grandphares', 'on');
+						$('#BtnGrands').addClass(activeClass);
+					}
+					break;
+			case 65: //A
+					socket.emit('grandphares', 'on');
+					$('#BtnGrands').addClass(activeClass);
+					break;
+		}
+	});
 
-    switch(e.which){
-	case 69:
-	  socket.emit('setled2', 'off');
-	  break;
-	case 81:
-	case 68:
-	  socket.emit('setservo', 'half');
-	  break
-    }
-  });
+	$(document).keyup(function(e){
+
+		switch(e.which){
+			case 65: //A
+				socket.emit('grandphares', 'off');
+				$('#BtnGrands').removeClass(activeClass);
+				break;
+			case 81: //Q
+			case 68: //D
+				socket.emit('setdirection', 'neutre');
+				$('#BtnGauche').removeClass(activeClass);
+				$('#BtnDroite').removeClass(activeClass);
+				break;
+			case 83: //S
+			case 90: //Z
+				socket.emit('feustop','on');
+				socket.emit('setpropulsion', 'stop');
+				socket.emit('feurecul','off');
+				$('#BtnRecule').removeClass(activeClass);
+				$('#BtnAvance').removeClass(activeClass);
+				break;
+		}
+	});
+
+	socket.on('log', function(content){
+		var dt = new Date();
+		var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds()+ '.'+dt.getMilliseconds();
+		$('#logconsole').val(time+' '+content+'\n'+$('#logconsole').val());
+	});
 });
